0e8ec9d7f37869cfcf5d72db93b48a4686d27338
ZJam <jam@zjamdemacbookair.local>
2018-05-23 18:46:13 +0800

    Initial Commit

---
+++ PushWithAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PushWithAnimation
+//
+//  Created by ZJam on 2018/5/23.
+//  Copyright © 2018年 ZJam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PushWithAnimation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PushWithAnimation
+//
+//  Created by ZJam on 2018/5/23.
+//  Copyright © 2018年 ZJam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
